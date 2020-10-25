cbe65f0982e21d7713b83587cd53f89ac60ba3d1
Jordan Reiser <jordanreiser@Jordans-MBP.fios-router.home>
2020-10-25 15:24:10 -0400

    Finished simple model with average 82% accuracy in final grades. Do more tests to up accuracy.

--- test.py
+++ test.py
@@ -13,4 +13,14 @@ predict = "G3"
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+#storing the best fit line using the linear regression model
+linear = linear_model.LinearRegression()
+
+#uses the x and y data to train the AI and determine a line of best fit for the data
+linear.fit(x_train, y_train)
+
+#stores the test results based of the training data
+accuracy = linear.score(x_test, y_test)
+print(accuracy)
