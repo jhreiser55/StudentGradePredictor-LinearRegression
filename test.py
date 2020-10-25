b0870057f47c9ec73b99f39ebaaecd9b55c81a4c
jhreiser <jordanreiser1255@gmail.com>
2020-10-25 15:54:41 -0400

    Ditching having to retrain everytime and solving based off previous runs.

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
 
 data = pd.read_csv("student-mat.csv", sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
@@ -15,8 +18,10 @@ y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-#storing the best fit line using the linear regression model
-linear = linear_model.LinearRegression()
+#commenting out so we dont train the model every time and can use previous knowledge with pickle
+#Use the commented out code when not using pickle
+#instanciaing the linear model
+"""linear = linear_model.LinearRegression()
 
 #uses the x and y data to train the AI and determine a line of best fit for the data
 linear.fit(x_train, y_train)
@@ -25,6 +30,13 @@ linear.fit(x_train, y_train)
 accuracy = linear.score(x_test, y_test)
 print(accuracy)
 
+#writing our results to a file to use later on
+with open("studentmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)"""
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
 print("Coefficient: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
 
@@ -32,3 +44,4 @@ print("Intercept: \n", linear.intercept_)
 predictions = linear.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
